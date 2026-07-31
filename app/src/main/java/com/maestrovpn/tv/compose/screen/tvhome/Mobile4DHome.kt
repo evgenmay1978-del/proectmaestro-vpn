8d1a4311bb220c3496522a7224e6d4c3f71a3fa4
sztushino-debug <sztushino@gmail.com>
2026-07-31 19:00:54 +0300

    fix: import Compose zIndex from correct package

--- app/src/main/java/com/maestrovpn/tv/compose/screen/tvhome/Mobile4DHome.kt
+++ app/src/main/java/com/maestrovpn/tv/compose/screen/tvhome/Mobile4DHome.kt
@@ -23,7 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.draw.zIndex
+import androidx.compose.ui.zIndex
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.BlendMode
