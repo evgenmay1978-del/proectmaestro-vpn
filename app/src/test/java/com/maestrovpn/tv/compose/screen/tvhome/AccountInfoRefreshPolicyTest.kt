7b1feb1366b05dffa66e57edc60c34a46ce5be7b
sztushino-debug <sztushino@gmail.com>
2026-08-13 12:04:41 +0300

    test(mobile): bind cached owner state to subscription

--- app/src/test/java/com/maestrovpn/tv/compose/screen/tvhome/AccountInfoRefreshPolicyTest.kt
+++ app/src/test/java/com/maestrovpn/tv/compose/screen/tvhome/AccountInfoRefreshPolicyTest.kt
@@ -4,28 +4,56 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class AccountInfoRefreshPolicyTest {
-    @Test
-    fun failedRefreshKeepsLastKnownOwnerIdentityWhenLocalProfileStillExists() {
-        val previous = AccountInfo(
-            login = "wapmix",
-            daysLeft = 31,
-            hasSubProfile = true,
-            expiresDate = "13.09.2026",
-        )
-        val failedRefresh = AccountInfo(hasSubProfile = true)
+    private val owner = AccountInfo(
+        login = "wapmix",
+        daysLeft = 31,
+        hasSubProfile = true,
+        expiresDate = "13.09.2026",
+        subscriptionIdentity = "profile-7:owner-token-hash",
+    )
 
-        assertEquals(previous, accountInfoAfterRefresh(previous, failedRefresh))
+    @Test
+    fun failedRefreshKeepsLastKnownOwnerOnlyForSameTrustedSubscriptionOnMobile() {
+        val failedRefresh = AccountInfo(
+            hasSubProfile = true,
+            subscriptionIdentity = owner.subscriptionIdentity,
+        )
+
+        assertEquals(owner, accountInfoAfterRefresh(owner, failedRefresh, preserveVerifiedIdentityOnOutage = true))
+    }
+
+    @Test
+    fun replacingOwnerSubscriptionDoesNotKeepPrivateOwnerIdentity() {
+        val replacement = AccountInfo(
+            hasSubProfile = true,
+            subscriptionIdentity = "profile-8:ordinary-token-hash",
+        )
+
+        assertEquals(replacement, accountInfoAfterRefresh(owner, replacement, preserveVerifiedIdentityOnOutage = true))
+    }
+
+    @Test
+    fun unrelatedLocalSubscriptionDoesNotKeepPrivateOwnerIdentity() {
+        val noTrustedProfile = AccountInfo(hasSubProfile = true)
+
+        assertEquals(noTrustedProfile, accountInfoAfterRefresh(owner, noTrustedProfile, preserveVerifiedIdentityOnOutage = true))
+    }
+
+    @Test
+    fun tvNeverKeepsOwnerIdentityAcrossInfoFailure() {
+        val failedRefresh = AccountInfo(
+            hasSubProfile = true,
+            subscriptionIdentity = owner.subscriptionIdentity,
+        )
+
+        assertEquals(failedRefresh, accountInfoAfterRefresh(owner, failedRefresh, preserveVerifiedIdentityOnOutage = false))
     }
 
     @Test
     fun removingLocalSubscriptionClearsLastKnownIdentity() {
-        val previous = AccountInfo(
-            login = "wapmix",
-            daysLeft = 31,
-            hasSubProfile = true,
-            expiresDate = "13.09.2026",
+        assertEquals(
+            AccountInfo(),
+            accountInfoAfterRefresh(owner, AccountInfo(), preserveVerifiedIdentityOnOutage = true),
         )
-
-        assertEquals(AccountInfo(), accountInfoAfterRefresh(previous, AccountInfo()))
     }
 }
